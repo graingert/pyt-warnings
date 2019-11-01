7d10b1e7b5d9a58703bb04bbe5e9ccfa345235d0
Thomas Grainger <tagrain@gmail.com>
2019-11-01 11:01:55 +0000

    pytest warn failure

---
+++ tests/test_foo.py
@@ -0,0 +1,21 @@
+import warnings
+
+import pytest
+
+class MyWarning(Warning):
+    pass
+
+def do_warning():
+    warnings.warn("bad", MyWarning)
+
+@pytest.mark.xfail(reason="should raise a MyWarning")
+def test_1():
+    do_warning()
+
+@pytest.mark.filterwarnings("ignore:bad:" + MyWarning.__module__ + "." + MyWarning.__name__)
+def test_2():
+    do_warning()
+
+@pytest.mark.xfail(reason="should raise a MyWarning")
+def test_3():
+    do_warning()
